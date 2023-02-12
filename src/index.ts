d3c8dbd83cd5b9ad31044b0e768e3b593700d41b
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2023-02-12 07:14:32 -0300

    Return original string if unable to parse XML

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ import { formatter } from "./recursiveformatter";
 import { getJSONFromXML } from './convert';
 
 export function formatXMLString(input: string, tabSize: number, lineWidth: number): string {
-  return formatter(getJSONFromXML(input), ' '.repeat(tabSize), lineWidth);
+  try {
+    return formatter(getJSONFromXML(input), ' '.repeat(tabSize), lineWidth);
+  } catch (er) {
+    return input;
+  }
 }
