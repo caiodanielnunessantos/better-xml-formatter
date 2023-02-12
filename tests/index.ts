9194c728df9d745b9f9fd7ac662913ddd41679b2
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2023-02-12 05:22:57 -0300

    Fix test

--- tests/index.ts
+++ tests/index.ts
@@ -6,6 +6,6 @@ require('./recursiveformatter');
 
 import { readFileSync, writeFileSync } from "fs";
 import { formatXMLString } from "../src";
-formatXMLString('', { tabSize: 3 });
+//formatXMLString('', { tabSize: 3 });
 //writeFileSync('./b.xmlaasdfa', formatXMLString(readFileSync('./a.xmlaasdfa').toString()));
 //console.log(formatXMLString(readFileSync('./a.xml').toString()));
